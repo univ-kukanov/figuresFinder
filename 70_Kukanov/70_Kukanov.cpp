e14daac537f5e30460f462c86fd3c7e7d8d26896
Boris <boriskukanov2005@gmail.com>
2025-06-01 15:26:22 +0300

    feat(70_Kukanov.cpp): добавлены заголовки всех функций

--- 70_Kukanov/70_Kukanov.cpp
+++ 70_Kukanov/70_Kukanov.cpp
@@ -1,1 +1,40 @@
 ﻿#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+void readDataFromFile(string filename, vector<string>& lines, set<Error>& errors)
+{
+
+}
+
+int* parseMatrixData(vector<string>& lines, set<Error>& errors, int* numberOfRows, int* numberOfColumns, int* maxElementSize)
+{
+    return NULL;
+}
+
+bool outputDataToFile(string filename, vector<string>& output, set<Error>& errors)
+{
+    return true;
+}
+
+void extractLargestFiguresFromMatrix(int* matrix, int numberOfRows, int	numberOfColumns, set<Figure>& figures)
+{
+
+}
+
+void findFigureInMatrixByGivenElement(int* matrix, int* passedMatrix, int row, int column, int element, Figure& newFigure, int numberOfRows, int numberOfColumns)
+{
+
+}
+
+void generateOutputMatrix(set<Figure>& figures, vector<string>& output, int maxElementSize, int numberOfRows, int numberOfColumns)
+{
+
+}
