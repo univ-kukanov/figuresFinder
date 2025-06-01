2c0b6902db49ac97b9c49c39864b59bcf72f8905
Boris <boriskukanov2005@gmail.com>
2025-06-01 23:42:28 +0300

    feat(main): добавлен код для тестового запуска программы

--- 70_Kukanov/70_Kukanov.cpp
+++ 70_Kukanov/70_Kukanov.cpp
@@ -9,9 +9,47 @@
 
 using namespace std;
 
+void readDataFromFile(string filename, vector<string>& lines, set<Error>& errors);
+int* parseMatrixData(vector<string>& lines, set<Error>& errors, int* numberOfRows, int* numberOfColumns, int* maxElementSize);
+bool outputDataToFile(string filename, vector<string>& output, set<Error>& errors);
+void extractLargestFiguresFromMatrix(int* matrix, int numberOfRows, int	numberOfColumns, set<Figure>& figures);
+void findFigureInMatrixByGivenElement(int* matrix, int* passedMatrix, int row, int column, int element, Figure& newFigure, int numberOfRows, int numberOfColumns);
+void generateOutputMatrix(set<Figure>& figures, vector<string>& output, int maxElementSize, int numberOfRows, int numberOfColumns);
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int* matrix = new int[100]{
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 1
+    };
+
+    int maxElementSize = 1;
+    int numberOfRows = 10;
+    int numberOfColumns = 10;
+    set<Figure> figures;
+    set<Error> errors;
+    vector<string> output;
+    string outputFilename = "./test.txt";
+
+    extractLargestFiguresFromMatrix(matrix, numberOfRows, numberOfColumns, figures);
+    if (figures.size() > 0) {
+        generateOutputMatrix(figures, output, maxElementSize, numberOfRows, numberOfColumns);
+    }
+    else {
+        output.push_back("no figures");
+    }
+
+    outputDataToFile(outputFilename, output, errors);
+    return 1;
 }
 
 void readDataFromFile(string filename, vector<string>& lines, set<Error>& errors)
