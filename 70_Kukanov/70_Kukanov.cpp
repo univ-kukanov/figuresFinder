a26252c079302c9f179eb9f36c17a35a18a5dcdc
Boris <boriskukanov2005@gmail.com>
2025-06-01 15:50:44 +0300

    feat(extractLargestFiguresFromMatrix): написан код для функции extractLargestFiguresFromMatrix

--- 70_Kukanov/70_Kukanov.cpp
+++ 70_Kukanov/70_Kukanov.cpp
@@ -1,6 +1,9 @@
 ﻿#include <iostream>
 #include <set>
 #include <vector>
+#include "ElementPosition.h"
+#include "Error.h"
+#include "Figure.h"
 
 using namespace std;
 
@@ -26,7 +29,31 @@ bool outputDataToFile(string filename, vector<string>& output, set<Error>& error
 
 void extractLargestFiguresFromMatrix(int* matrix, int numberOfRows, int	numberOfColumns, set<Figure>& figures)
 {
+    int largestFigureSize = 2;
+    int* passedMatrix = new int[numberOfRows * numberOfColumns];
+    fill_n(passedMatrix, numberOfRows * numberOfColumns, 0);
 
+    for (int i = 0; i < numberOfRows; i++) {
+        for (int j = 0; j < numberOfColumns; j++) {
+            if ((passedMatrix + i * numberOfColumns + j) == 0) {
+                Figure newFigure;
+                int newElement = *(matrix + i * numberOfColumns + j);
+
+                findFigureInMatrixByGivenElement(matrix, passedMatrix, i, j, newElement, newFigure, numberOfRows, numberOfColumns);
+
+                int newFigureSize = newFigure.figureSize();
+                if (newFigureSize > largestFigureSize) {
+                    figures.clear();
+                    figures.insert(newFigure);
+
+                    largestFigureSize = newFigureSize;
+                }
+                else if (newFigureSize == largestFigureSize) {
+                    figures.insert(newFigure);
+                }
+            }
+        }
+    }
 }
 
 void findFigureInMatrixByGivenElement(int* matrix, int* passedMatrix, int row, int column, int element, Figure& newFigure, int numberOfRows, int numberOfColumns)
