1a71e230e84d925e062d679c6dc9b73eed95e8ae
Boris <boriskukanov2005@gmail.com>
2025-06-02 14:52:22 +0300

    refactor(70_Kukanov.cpp): все прототипы функций перенесены в новый заголовочный файл

--- 70_Kukanov/70_Kukanov.cpp
+++ 70_Kukanov/70_Kukanov.cpp
@@ -9,15 +9,6 @@
 
 using namespace std;
 
-void readDataFromFile(string filename, vector<string>& lines, set<Error>& errors);
-int* parseMatrixData(vector<string>& lines, set<Error>& errors, int* numberOfRows, int* numberOfColumns, int* maxElementSize);
-bool outputDataToFile(string filename, vector<string>& output, set<Error>& errors);
-void extractLargestFiguresFromMatrix(int* matrix, int numberOfRows, int	numberOfColumns, set<Figure>& figures);
-void findFigureInMatrixByGivenElement(int* matrix, int* passedMatrix, int row, int column, int element, Figure& newFigure, int numberOfRows, int numberOfColumns);
-void generateOutputMatrix(set<Figure>& figures, vector<string>& output, int maxElementSize, int numberOfRows, int numberOfColumns);
-bool isInIntRange(string number);
-
-
 int main(int argc, char* argv[])
 {
     if (argc < 3)
